8c8ea1539b95bba8074de1a90c30198b96370573
athnme <a.oezbanazi@gmail.com>
2020-11-06 16:57:16 +0100

    Finalize header styled-components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
-import "./Header.css";
+import styled from "styled-components";
 import React from "react";
+
+const HeaderStyled = styled.div`
+  background-color: var(--background-color);
+  background: rgb(49, 10, 107);
+  background: linear-gradient(
+    180deg,
+    rgba(49, 10, 107, 1) 0%,
+    rgba(102, 51, 153, 1) 100%
+  );
+  color: var(--foreground-color);
+  text-align: center;
+`;
 
 export default function Header() {
   return (
-    <div className="header">
-      <h1 className="header__titel">-= Real 🎯 Decide =-</h1>
-      <h5 className="header__subtitel"> localStorage(📦) - Version</h5>
-    </div>
+    <HeaderStyled>
+      <h1
+        css={`
+          margin-top: 4rem;
+        `}
+      >
+        -= Real 🎯 Decide =-
+      </h1>
+      <h5
+        css={`
+          margin-bottom: 5rem;
+        `}
+      >
+        {" "}
+        localStorage(📦) - Version
+      </h5>
+    </HeaderStyled>
   );
 }

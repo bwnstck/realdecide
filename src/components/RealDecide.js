e2d41cde48fa0a2841fd06e48a132df0db9832c2
athnme <a.oezbanazi@gmail.com>
2020-11-06 17:30:04 +0100

    Lucky One finalize styled-component

--- src/components/RealDecide.js
+++ src/components/RealDecide.js
@@ -1,11 +1,20 @@
 import "./RealDecide.css";
-
+import styled from "styled-components";
 import React from "react";
 import Value from "./Value";
 
+const RealDecideStyled = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  color: black;
+  padding: 0;
+  margin: 1rem auto;
+`;
+
 export default function Realdecide({ values, setValues }) {
   return (
-    <ul className="RealDecide">
+    <RealDecideStyled className="RealDecide">
       {values.map((value) => (
         <Value
           key={value.id}
@@ -14,6 +23,6 @@ export default function Realdecide({ values, setValues }) {
           setValues={setValues}
         />
       ))}
-    </ul>
+    </RealDecideStyled>
   );
 }
--- src/components/TheLuckyOne.js
+++ src/components/TheLuckyOne.js
@@ -1,11 +1,51 @@
-import "./TheLuckyOne.css";
+import styled from "styled-components";
 import React from "react";
+
+const LuckyOneContainer = styled.div`
+  margin: 1rem auto;
+  padding: 1rem 0rem 0;
+`;
+
+const LuckyOneText = styled.h2`
+  display: inline-block;
+  margin: 0.5rem auto 3rem;
+  color: var(--action-text);
+  background-color: var(--action-text);
+  padding: 0.5rem;
+  text-shadow: 0 -2 5px var(--background-dark),
+    0px 2px 5px var(--background-dark), -2px -2px var(--background-dark);
+  border-radius: 5px;
+  background: linear-gradient(
+    45deg,
+    #ff0000,
+    #ff7300,
+    #fffb00,
+    #48ff00,
+    #00ffd5,
+    #002bff,
+    #7a00ff,
+    #ff00c8,
+    #ff0000
+  );
+  background-size: 400%;
+  animation: glowing 20s linear infinite;
+  transition: glowing 0.3s ease-in-out;
+  border-radius: 10px;
+`;
 
 export default function TheLuckyOne({ theLuckyOne }) {
   return (
-    <div className="theLuckyOne__container unMarkable">
-      <h3 className="theLuckyOne__itsYou">🚀 Its you! 👩‍🎤</h3>
-      <h2 className="theLuckyOne__text  glow">✨ {theLuckyOne} ✨</h2>
-    </div>
+    <LuckyOneContainer>
+      <h3
+        css={`
+          margin: 1rem 0 auto;
+        `}
+      >
+        🚀 Its you! 👩‍🎤
+      </h3>
+      <LuckyOneText className="theLuckyOne__text  glow">
+        ✨ {theLuckyOne} ✨
+      </LuckyOneText>
+    </LuckyOneContainer>
   );
 }
